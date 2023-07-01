4860a0761b56632fadfa26b9829e63fe98821d25
danrealednb <realed@dnb.com>
2023-07-01 00:16:00 -0400

    fixed search to add clear

--- bourbon-book/app/components/spirits/SpiritFilterBox.tsx
+++ bourbon-book/app/components/spirits/SpiritFilterBox.tsx
@@ -1,18 +1,41 @@
-import { Form, useSearchParams } from "@remix-run/react";
+import { Form, useSearchParams, useNavigate } from "@remix-run/react";
+import { useState } from "react";
 
 function SpiritFilterBox() {
+  const navigate = useNavigate();
+
   const [params] = useSearchParams();
+
+  const handleChange = () => {
+    document.getElementById("query").value = "";
+    params.delete("filter");
+    navigate("/spirits");
+  };
 
   return (
     <>
-      <Form className="flex justify-center pt-5">
-        <input
-          type="text"
-          name="query"
-          placeholder="Search Spirits"
-          defaultValue={params.get("query") || ""}
-        ></input>
-      </Form>
+      <div className="flex justify-center pt-5 space-x-2 items-center">
+        <Form className="flex space-x-2">
+          <input
+            id="query"
+            type="text"
+            name="query"
+            placeholder="Search Spirits"
+            className="rounded border-2 border-white"
+          />
+          <button className="text-white border-2 rounded px-1">Search</button>
+        </Form>
+        {params.get("query") && (
+          <button
+            id="clearBtn"
+            name="clearBtn"
+            className="text-white border-2 rounded px-1"
+            onClick={handleChange}
+          >
+            Clear
+          </button>
+        )}
+      </div>
     </>
   );
 }
